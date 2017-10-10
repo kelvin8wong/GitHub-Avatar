67e95c886d2594c10dbd5095ce114d72671e1b9e
kelvin8wong <kelvin8wong@gmail.com>
2017-10-10 02:24:25 +0000

    tested requestURL and worked

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,15 @@
 var request = require('request');
 
+//introductoion
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  console.log (repoName);
-  console.log (repoOwner);
+  var GITHUB_USER = "kelvin8wong";
+  var GITHUB_TOKEN = "1d088ff710e91bed8f00d83ebe967d778dbe1bb2";
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
