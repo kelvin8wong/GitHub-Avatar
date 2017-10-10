0655a03654e57b82335f7cf46153963b63186985
kelvin8wong <kelvin8wong@gmail.com>
2017-10-10 01:49:07 +0000

    getRepoContributors cb function implemented and worked

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  console.log (repoName);
+  console.log (repoOwner);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
