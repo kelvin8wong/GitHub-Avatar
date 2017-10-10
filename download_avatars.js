95dfa534262542216ee3f04268f5d9f1c4526b4f
kelvin8wong <kelvin8wong@gmail.com>
2017-10-10 02:55:17 +0000

    added user agent

--- download_avatars.js
+++ download_avatars.js
@@ -8,7 +8,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_TOKEN = "1d088ff710e91bed8f00d83ebe967d778dbe1bb2";
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  //console.log(requestURL); to test requestURL
+
+  //pass custom user-agent string to access API
+  const options = {
+    url: requestURL,
+    headers: {
+    'User-Agent': 'GitHub Avatar Downloader - Student Project'
+  }
+
+  request(options, cb)
 
 }
 
